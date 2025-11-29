fa5c3e00e4da72b83720065986f95510e78c1ca8
tthrall <tthrall@Alumni.Stanford.edu>
2025-11-29 12:52:21 +0000

    Rename wine_quality variables to abbreviated underscore format

--- data-raw/wine_quality.R
+++ data-raw/wine_quality.R
@@ -11,11 +11,19 @@ url_white <- "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-qua
 red   <- read_delim(url_red,   delim = ";", show_col_types = FALSE)
 white <- read_delim(url_white, delim = ";", show_col_types = FALSE)
 
-# Combine with color indicator
+# Combine with color indicator and rename variables
 wine_quality <- bind_rows(
   red   |> mutate(color = "red"),
   white |> mutate(color = "white")
 ) |>
+  rename(
+    fix_acidity = `fixed acidity`,
+    vol_acidity = `volatile acidity`,
+    citric_acid = `citric acid`,
+    res_sugar   = `residual sugar`,
+    free_so2    = `free sulfur dioxide`,
+    total_so2   = `total sulfur dioxide`
+  ) |>
   relocate(color) |>
   mutate(color = factor(color))
 
